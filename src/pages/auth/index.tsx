35803d56ff2ffa10cece17478834937142447f5a
shah9380 <130676464+shah9380@users.noreply.github.com>
2024-02-08 22:44:16 +0530

    Navbar

---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,15 @@
+
+import Navbar from '../../components/Navbar/Navbar';
+import React from 'react';
+
+type AuthPageProps = {
+    
+};
+
+const AuthPage:React.FC<AuthPageProps> = () => {
+    
+    return <div className='bg-black'>
+        <Navbar></Navbar>
+    </div>
+}
+export default AuthPage;
