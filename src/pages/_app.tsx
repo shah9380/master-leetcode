911f82a5ad8c3209d6c443f38373c86d1714d2eb
shah9380 <130676464+shah9380@users.noreply.github.com>
2024-02-08 22:30:22 +0530

    added icond and pngs in public folder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,10 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { RecoilRoot } from "recoil";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+
 export default function App({ Component, pageProps }: AppProps) {
-	return (
-		<RecoilRoot>
-			<Head>
+  return <>
+        <Head>
 				<title>LeetClone</title>
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<link rel='icon' href='/favicon.png' />
@@ -16,8 +13,6 @@ export default function App({ Component, pageProps }: AppProps) {
 					content='Web application that contains leetcode problems and video solutions'
 				/>
 			</Head>
-			<ToastContainer />
-			<Component {...pageProps} />
-		</RecoilRoot>
-	);
+        <Component {...pageProps} />
+  </>;
 }
