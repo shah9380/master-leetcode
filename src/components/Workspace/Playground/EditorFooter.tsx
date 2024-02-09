9f11c2ad731b935b458aee81d3845c246b344b05
shah9380 <130676464+shah9380@users.noreply.github.com>
2024-02-09 23:41:04 +0530

    changes for run button

--- src/components/Workspace/Playground/EditorFooter.tsx
+++ src/components/Workspace/Playground/EditorFooter.tsx
@@ -19,7 +19,7 @@ const EditorFooter:React.FC<EditorFooterProps> = ({handleSubmit}) => {
                     </button>
                 </div>
                 <div className='ml-auto flex items-center space-x-4'>
-                    <button className='px-3 py-1.5 text-sm font-medium items-center whitespace-nowrap transition-all focus:outline-none inline-flex bg-dark-fill-3  hover:bg-dark-fill-2 text-dark-label-2 rounded-lg'>
+                    <button className='px-3 py-1.5 text-sm font-medium items-center whitespace-nowrap transition-all focus:outline-none inline-flex bg-dark-fill-3  hover:bg-dark-fill-2 text-dark-label-2 rounded-lg' onClick={handleSubmit}>
                         Run
                     </button>
                     <button className='px-3 py-1.5 font-medium items-center transition-all focus:outline-none inline-flex text-sm text-white bg-dark-green-s hover:bg-green-3 rounded-lg' onClick={handleSubmit}>
